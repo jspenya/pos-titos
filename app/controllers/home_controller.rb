cb467bf1bb5dd64073097f4727093f1b55614dc4
pacianoseth <feintimg@gmail.com>
2023-10-11 14:27:30 +0800

    nel ikaw na bahala sa sumpay

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
 class HomeController < ApplicationController
     def homepage
+        @category = Category.all
     end
     def dashboard
     end
-    
+
+    def show
+        @product = Category.find_by(id:params[:id]).products
+        rescue ActiveRecord::RecordNotFound
+            redirect_to root_path
+    end
+
 end
