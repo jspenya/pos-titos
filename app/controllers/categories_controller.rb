692c13f6472105c9c0245e3804e547a3a018d15d
Neil Oscar Hawan <71697673+NeilOscarHawan@users.noreply.github.com>
2023-10-25 01:13:56 +0800

    Feature/add crud on product and category (#5)
    * added model products and category relationships
    
    * nel ikaw na bahala sa sumpay
    
    * pa debug sa master! :D
    
    * separate categories from home controller
    
    * gitMerge github.com:jspenya/pos-titos into added_show_category
    
    * fix: bin updates
    
    - remove tailwind from app
    - fix bin/rails generated by bundler
    
    * style updates & form fixes
    
    ---------
    
    Co-authored-by: pacianoseth <feintimg@gmail.com>
    Co-authored-by: Jose Stephen Peña <pena.jstephen@gmail.com>

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,54 @@
+class CategoriesController < ApplicationController
+  # GET requests
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    @products = @category.products
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to @category
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def new
+    @category = Category.new
+  end
+  # End of GET requests
+
+  def create
+    @category = Category.new(category_params)
+
+    respond_to do |format|
+      if @category.save
+        flash[:notice] = "Category was successfully created."
+        format.html { redirect_to categories_path }
+        format.json { render :show, status: :created, location: @category }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
-  root 'home#homepage'
-  get 'homepage', to: 'home#homepage'
-  get 'dashboard', to: 'home#dashboard'
+  root 'categories#index'
+
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :categories do
+    resources :products
+  end
 end
