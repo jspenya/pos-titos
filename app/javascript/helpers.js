fe073142f71ef0457f40c5adf102244ea530740d
Jose Stephen Peña <pena.jstephen@gmail.com>
2023-10-30 14:05:22 +0800

    add dropzonejs to products form

---
+++ app/javascript/helpers.js
@@ -0,0 +1,32 @@
+export function getMetaValue(name) {
+  const element = findElement(document.head, `meta[name="${name}"]`)
+  if (element) {
+    return element.getAttribute("content")
+  }
+}
+
+export function findElement(root, selector) {
+  if (typeof root == "string") {
+    selector = root
+    root = document
+  }
+  return root.querySelector(selector)
+}
+
+export function removeElement(el) {
+  if (el && el.parentNode) {
+    el.parentNode.removeChild(el);
+  }
+}
+
+export function insertAfter(el, referenceNode) {
+  return referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
+}
+
+export function arrayToSentence(arr) {
+  return arr.join(", ").replace(/,\s([^,]+)$/, ' and $1');
+}
+
+export function toSentence(str) {
+  return str.split("_").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+}
