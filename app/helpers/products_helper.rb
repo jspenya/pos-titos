7c8388b228acad592845dad055d5fe7978ef5527
Jose Stephen Peña <pena.jstephen@gmail.com>
2023-10-30 14:05:22 +0800

    add dropzonejs to products form

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,9 @@
 module ProductsHelper
+  def product_form_opts(category, product)
+    if product.new_record?
+      { model: [ category, category.products.build ], class: "w-100", multipart: true }
+    else
+      { model: product, url: category_product_path(category), method: :patch, local: true, multipart: true }
+    end
+  end
 end
