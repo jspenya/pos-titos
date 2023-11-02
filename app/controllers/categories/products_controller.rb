5acd8901db6dd4dffde320dbca3a50202a9e57ca
Jose Stephen Peña <pena.jstephen@gmail.com>
2023-11-02 13:01:26 +0800

    feat: add products crud & app sidebar

---
+++ app/controllers/categories/products_controller.rb
@@ -0,0 +1,66 @@
+module Categories
+  class ProductsController < ProductsController
+    before_action :get_category
+    before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+    def index
+      @products = @category.products
+    end
+
+    def show; end
+
+    def new
+      @product = @category.products.build
+    end
+
+    def create
+      @category = Category.find(params[:category_id])
+      @product = @category.products.build(product_params)
+
+      respond_to do |format|
+        if @product.save
+          format.html { redirect_to category_path(@category), notice: 'Product was successfully created.' }
+          format.json { render :show, status: :created, location: @product }
+        else
+          format.html { render :new }
+          format.json { render json: @product.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    def edit; end
+
+    def update
+      respond_to do |format|
+        if @product.update(product_params)
+          format.html { redirect_to category_path(@category), notice: 'Product was successfully updated.' }
+          format.json { render :show, status: :ok, location: @product }
+        else
+          format.html { render :edit }
+          format.json { render json: @product.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    def destroy
+      @product = @category.products.find(params[:id])
+      @product.destroy
+      redirect_to category_path(@category), notice: 'Product was successfully destroyed.'
+    end
+
+    private
+
+
+    def set_product
+      @product = @category.products.find(params[:id])
+    end
+
+    def product_params
+      params.require(:product).permit(:name, :quantity, :price, :category_id, :image)
+    end
+
+    def get_category
+      @category= Category.find(params[:category_id])
+    end
+  end
+end
