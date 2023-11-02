9c754fc89e20cbf67bd9e62a51b12367b753d376
Jose Stephen Peña <pena.jstephen@gmail.com>
2023-11-02 13:01:26 +0800

    feat: add products crud & app sidebar

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,5 +1,5 @@
 module ProductsHelper
-  def product_form_opts(category, product)
+  def product_form_opts(category = nil, product)
     if product.new_record?
       { model: [ category, category.products.build ], class: "w-100", multipart: true }
     else
