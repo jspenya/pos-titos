6f8d8d50702b907ba0d376102ecf1cc35f85fc63
Jose Stephen Peña <pena.jstephen@gmail.com>
2023-11-02 13:01:26 +0800

    feat: add products crud & app sidebar

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,24 +1,22 @@
 class ProductsController < ApplicationController
-  before_action :get_category
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   def index
-    @products = @category.products
+    @products = Product.all
   end
 
   def show; end
 
   def new
-    @product = @category.products.build
+    @product = Product.new
   end
 
   def create
-    @category = Category.find(params[:category_id])
-    @product = @category.products.build(product_params)
+    @product = Product.new(product_params)
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to category_path(@category), notice: 'Product was successfully created.' }
+        format.html { redirect_to products_path, notice: 'Product was successfully created.' }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new }
@@ -32,7 +30,7 @@ class ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to category_path(@category), notice: 'Product was successfully updated.' }
+        format.html { redirect_to products_path, notice: 'Product was successfully updated.' }
         format.json { render :show, status: :ok, location: @product }
       else
         format.html { render :edit }
@@ -42,24 +40,17 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    @product = @category.products.find(params[:id])
     @product.destroy
-    redirect_to category_path(@category), notice: 'Product was successfully destroyed.'
+    redirect_to products_path, notice: 'Product was successfully destroyed.'
   end
-
-
 
   private
 
-    def set_product
-      @product = @category.products.find(params[:id])
-    end
+  def set_product
+    @product = Product.find(params[:id])
+  end
 
-    def product_params
-      params.require(:product).permit(:name, :quantity, :price, :category_id, :image)
-    end
-
-    def get_category
-      @category= Category.find(params[:category_id])
-    end
+  def product_params
+    params.require(:product).permit(:name, :quantity, :price, :category_id, :image)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
-  root 'categories#index'
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "products#index"
 
   resources :categories do
-    resources :products
+    resources :products, module: "categories"
   end
+
+  resources :products
 end
