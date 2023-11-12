7af537ad68e9e9d7dc29681257c3f8db1154bf81
Jose Stephen Peña <pena.jstephen@gmail.com>
2023-11-12 15:05:02 +0800

    fix indentation & update sidebar icon for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,48 +1,48 @@
 class OrdersController < ApplicationController
-before_action :set_order, only: [:show, :edit, :update, :destroy]
-    def index
-        @orders = Order.ordered
-    end
+  before_action :set_order, only: [:show, :edit, :update, :destroy]
 
-    def show
-    end
+  def index
+    @orders = Order.ordered
+  end
 
-    def new
-        @order = Order.new
-    end
+  def show
+  end
 
-    def create
-        @order = Order.new(orders_params)
-        if @order.save
-            respond_to do |format|
-                format.html { redirect_to orders_path, notice: "Order was successfully created." }
-                format.json { render :show, status: :created, location: @order }
-                format.turbo_stream
-            end
-        else
-            render :new, status: :unprocessable_entity
-        end
-    end
+  def new
+    @order = Order.new
+  end
 
-    def edit
-    end
-
-    def destroy
-        @order.destroy!
-    
-        respond_to do |format|
-          format.html { redirect_to orders_path, notice: "Order was successfully destroyed." }
-          format.turbo_stream
-        end
+  def create
+    @order = Order.new(orders_params)
+    if @order.save
+      respond_to do |format|
+        format.html { redirect_to orders_path, notice: "Order was successfully created." }
+        format.json { render :show, status: :created, location: @order }
+        format.turbo_stream
       end
-
-    private
-
-    def set_order
-        @order = Order.find(params[:id])
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    def orders_params 
-        params.require(:order).permit(:user_id ,:name, :status)
+  def edit; end
+
+  def destroy
+    @order.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to orders_path, notice: "Order was successfully destroyed." }
+      format.turbo_stream
     end
+  end
+
+  private
+
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  def orders_params
+    params.require(:order).permit(:user_id, :name, :status)
+  end
 end
