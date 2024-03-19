ba41d6fc77cae139325ba785d1342cfb449a06fc
Jose Peña <pena.jstephen@gmail.com>
2024-03-19 14:50:42 +0800

    remove destroy in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.destroy_all
 users = User.first_or_create([
   {
     username: 'admin',
