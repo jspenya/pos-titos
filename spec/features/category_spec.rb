bb14e3e139aff7648e59e2b4b4397176fbd4beb2
Jose Stephen Peña <pena.jstephen@gmail.com>
2024-11-01 01:33:43 +0800

    categories crud feature spec

--- spec/features/category_spec.rb
+++ spec/features/category_spec.rb
@@ -2,17 +2,48 @@ require 'rails_helper'
 
 RSpec.feature "Category", type: :feature do
   let(:admin) { FactoryBot.create(:user, :admin) }
-  let!(:category) { FactoryBot.create(:category, name: 'Beverage') }
+  let!(:category) { FactoryBot.create(:category, name: 'Beverages') }
+  let!(:category_edit) { FactoryBot.create(:category, name: 'Desserts') }
 
-  before { log_in(as: admin) }
-
-  scenario "User deletes a category" do
+  before do
+    log_in(as: admin)
     visit categories_path
+  end
 
-    within('.unit', match: :first) do
+  scenario "Displaying categories" do
+    expect(page).to have_content(category.name)
+    expect(page).to have_content(category_edit.name)
+  end
+
+  scenario "Deleting a category" do
+
+    within('.unit', text: category.name) do
       click_button 'Delete'
     end
 
     expect(page).to_not have_content(category.name)
+  end
+
+  scenario "Creating a new category" do
+    click_link 'New Category'
+
+    fill_in 'category_name', with: 'Foo!'
+    find('input[value="Create Category"]', match: :first).click
+
+    expect(page).to have_content('Foo!')
+  end
+
+  scenario "Editing an existing category" do
+    within('.unit', text: category_edit.name) do
+      click_link 'Edit Category'
+    end
+
+    category_name = find('input[id="category_name"]', visible: true)
+    expect(category_name.value).to eq(category_edit.name)
+
+    fill_in 'category_name', with: 'Edited!'
+    find('input[value="Update Category"]', visible: true).click
+
+    expect(page).to have_content('Edited!')
   end
 end
