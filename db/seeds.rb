00c83f0b40889c8880b0fe1e405d83f0548d8cfe
Jose Stephen Peña <pena.jstephen@gmail.com>
2024-11-11 00:55:22 +0800

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-users = User.first_or_create([
+
+# Seed users
+users = [
   {
     username: 'admin',
     email: 'admin@user.com',
@@ -33,20 +30,29 @@ users = User.first_or_create([
     first_name: 'stock',
     last_name: 'man'
   },
-])
+]
 
-categories = ['egm wings', 'dahon plates', 'drinks','rice toppings','pizza',
-              'finger food','appetizer','rice','alcohol']
-categories.each do |category_name|
-  Category.find_or_create_by(name: category_name)
+users.each do |user_attrs|
+  User.find_or_create_by!(email: user_attrs[:email]) do |user|
+    user.update!(user_attrs)
+  end
 end
 
+# Seed categories
+categories = ['egm wings', 'dahon plates', 'drinks', 'rice toppings', 'pizza',
+              'finger food', 'appetizer', 'rice', 'alcohol']
+
+categories.each do |category_name|
+  Category.find_or_create_by!(name: category_name)
+end
+
+# Seed products
 products = [
   { name: 'Buffalo Wings (8pcs)', price: 259, category_name: 'egm wings' },
-  { name: 'Garlic Parmesan', price: 259, category_name: 'egm wings'  },
-  { name: 'CDO Wings', price: 259, category_name: 'egm wings'  },
-  { name: 'Salted Egg', price: 259, category_name: 'egm wings'  },
-  { name: 'Barbecue', price: 259, category_name: 'egm wings'  },
+  { name: 'Garlic Parmesan', price: 259, category_name: 'egm wings' },
+  { name: 'CDO Wings', price: 259, category_name: 'egm wings' },
+  { name: 'Salted Egg', price: 259, category_name: 'egm wings' },
+  { name: 'Barbecue', price: 259, category_name: 'egm wings' },
   { name: '2pcs old fashion fried chicken', price: 219, category_name: 'dahon plates' },
   { name: '2pcs dahon spicy chicken', price: 249, category_name: 'dahon plates' },
   { name: 'CDO Wings', price: 219, category_name: 'dahon plates' },
@@ -79,36 +85,37 @@ products = [
   { name: 'Prawn Crackers', price: 139, category_name: 'finger food' },
   { name: 'Ding Dong Mixed Nuts', price: 49, category_name: 'finger food' },
   { name: 'Martys', price: 49, category_name: 'finger food' },
-  { name: 'Sizzling Sisig', price: 269, category_name: 'appetizer'},
-  { name: 'Sizzling Squid', price: 560, category_name: 'appetizer'},
-  { name: 'Kinilaw', price: 279, category_name: 'appetizer'},
-  { name: 'Siniglaw', price: 339, category_name: 'appetizer'},
-  { name: 'Calamari', price: 269, category_name: 'appetizer'},
-  { name: 'Dahon Fried Isaw', price: 149, category_name: 'appetizer'},
+  { name: 'Sizzling Sisig', price: 269, category_name: 'appetizer' },
+  { name: 'Sizzling Squid', price: 560, category_name: 'appetizer' },
+  { name: 'Kinilaw', price: 279, category_name: 'appetizer' },
+  { name: 'Siniglaw', price: 339, category_name: 'appetizer' },
+  { name: 'Calamari', price: 269, category_name: 'appetizer' },
+  { name: 'Dahon Fried Isaw', price: 149, category_name: 'appetizer' },
   { name: 'Barbecue Isaw', price: 149, category_name: 'appetizer' },
   { name: 'Crispy Pata', price: 899, category_name: 'appetizer' },
-  { name: 'Lechon Kawali', price: 490, category_name: 'appetizer'},
-  { name: 'Crispy Kangkong', price: 149, category_name: 'appetizer'},
-  { name: 'Chicken Skin', price: 159, category_name: 'appetizer'},
-  { name: 'Boneless chicken BBQ (3 PCS)', price: 289, category_name: 'appetizer'},
+  { name: 'Lechon Kawali', price: 490, category_name: 'appetizer' },
+  { name: 'Crispy Kangkong', price: 149, category_name: 'appetizer' },
+  { name: 'Chicken Skin', price: 159, category_name: 'appetizer' },
+  { name: 'Boneless chicken BBQ (3 PCS)', price: 289, category_name: 'appetizer' },
   { name: 'Rice Platter', price: 120, category_name: 'rice' },
-  { name: 'Rice Cup', price: 40, category_name: 'rice'},
-  { name: 'Java Rice', price: 69, category_name: 'rice'},
-  { name: 'Garlic Rice', price: 69, category_name: 'rice'},
-  { name: 'Solera', price: 1200, category_name: 'alcohol'},
-  { name: 'Fundador Light(1 Liter)', price: 1000, category_name: 'alcohol'},
-  { name: 'Fundador Light(700ml)', price: 800, category_name: 'alcohol'},
-  { name: 'Double Light(500ml)', price: 500, category_name: 'alcohol'},
-  { name: 'Mojitos Gold', price: 800, category_name: 'alcohol'},
-  { name: 'Captain Morgan', price: 1000, category_name: 'alcohol'},
-  { name: 'Captain Morgan Set', price: 1200, category_name: 'alcohol'},
-  { name: 'Bacardi-Gold', price: 1200, category_name: 'alcohol'},
-  { name: 'Bacardi-Set', price: 1400, category_name: 'alcohol'},
-  { name: 'Bacardi-Black', price: 1339, category_name: 'alcohol'}
-
-  # Add more products as needed
+  { name: 'Rice Cup', price: 40, category_name: 'rice' },
+  { name: 'Java Rice', price: 69, category_name: 'rice' },
+  { name: 'Garlic Rice', price: 69, category_name: 'rice' },
+  { name: 'Solera', price: 1200, category_name: 'alcohol' },
+  { name: 'Fundador Light(1 Liter)', price: 1000, category_name: 'alcohol' },
+  { name: 'Fundador Light(700ml)', price: 800, category_name: 'alcohol' },
+  { name: 'Double Light(500ml)', price: 500, category_name: 'alcohol' },
+  { name: 'Mojitos Gold', price: 800, category_name: 'alcohol' },
+  { name: 'Captain Morgan', price: 1000, category_name: 'alcohol' },
+  { name: 'Captain Morgan Set', price: 1200, category_name: 'alcohol' },
+  { name: 'Bacardi-Gold', price: 1200, category_name: 'alcohol' },
+  { name: 'Bacardi-Set', price: 1400, category_name: 'alcohol' },
+  { name: 'Bacardi-Black', price: 1339, category_name: 'alcohol' }
 ]
+
 products.each do |product_attrs|
-  category = Category.where(name: product_attrs[:category_name]).first
-  Product.first_or_create(name: product_attrs[:name], price: product_attrs[:price], category_id: category.id)
+  category = Category.find_by(name: product_attrs[:category_name])
+  Product.find_or_create_by!(name: product_attrs[:name], category_id: category.id) do |product|
+    product.price = product_attrs[:price]
+  end
 end
